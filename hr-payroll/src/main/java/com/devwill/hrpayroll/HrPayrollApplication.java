9b41be909cad594da9c8e4e3a45fe870d32e6b47
William Barbosa <will.barbosa23@gmail.com>
2024-11-09 01:40:55 -0300

    chore: Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/devwill/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,11 @@
+package com.devwill.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
+}
