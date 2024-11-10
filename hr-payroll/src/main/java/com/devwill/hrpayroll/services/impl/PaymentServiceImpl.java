bd20698e97b10f1d4a9c558b6b06f12898360256
William Barbosa <will.barbosa23@gmail.com>
2024-11-10 01:15:12 -0300

    feat: restTemplate implementation

--- hr-payroll/src/main/java/com/devwill/hrpayroll/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/com/devwill/hrpayroll/services/impl/PaymentServiceImpl.java
@@ -1,14 +1,33 @@
 package com.devwill.hrpayroll.services.impl;
 
 import com.devwill.hrpayroll.domain.Payment;
+import com.devwill.hrpayroll.domain.Worker;
 import com.devwill.hrpayroll.services.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    private RestTemplate restTemplate;
+
+    public PaymentServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
-    public Payment getPayment(long workerId, int days) {
-        return new Payment("Will", 1700.00, days);
+    public Payment getPayment(final long workerId, final int days) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", String.valueOf(workerId));
+
+        final var worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
