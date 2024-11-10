3d7f599009f68d734c4c8bffb03cce7399d52c6b
William Barbosa <will.barbosa23@gmail.com>
2024-11-10 02:50:39 -0300

    feat: randon port to hr-worker

--- hr-worker/src/main/java/com/devwill/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/devwill/hrworker/controllers/WorkerController.java
@@ -2,8 +2,6 @@ package com.devwill.hrworker.controllers;
 
 import com.devwill.hrworker.entities.Worker;
 import com.devwill.hrworker.repositories.WorkerRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +15,6 @@ import java.util.Optional;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
-    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
     private Environment env;
     private WorkerRepository repository;
@@ -37,8 +34,6 @@ public class WorkerController {
     public ResponseEntity<Optional<Worker>> findById(
             @PathVariable final Long id)
     {
-        logger.info("PORT = {}", env.getProperty("local.server.port"));
-
         final var worker = repository.findById(id);
         return ResponseEntity.ok(worker);
     }
