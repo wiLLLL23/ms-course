bfc60599c731b9da5c5cafcc9f695539c031070a
William Barbosa <will.barbosa23@gmail.com>
2024-11-11 22:11:43 -0300

    feat: Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/main/java/com/devwill/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,11 @@
+package com.devwill.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+    }
+}
