86455aec91a0b31e43119bf719b0bc573e3934dd
William Barbosa <will.barbosa23@gmail.com>
2024-11-11 23:15:41 -0300

    chore: project hr-config-server created

---
+++ hr-config-server/src/main/java/com/devwill/hrconfigserver/HrConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.devwill.hrconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrConfigServerApplication.class, args);
+    }
+}
