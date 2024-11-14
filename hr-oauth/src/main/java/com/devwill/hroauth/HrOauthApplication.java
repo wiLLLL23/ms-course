d96a95637c062df8491bd2e203b9a89abe346f87
William Barbosa <will.barbosa23@gmail.com>
2024-11-14 00:30:03 -0300

    feat: project hr-oauth created

---
+++ hr-oauth/src/main/java/com/devwill/hroauth/HrOauthApplication.java
@@ -0,0 +1,12 @@
+package com.devwill.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOauthApplication.class, args);
+    }
+}
