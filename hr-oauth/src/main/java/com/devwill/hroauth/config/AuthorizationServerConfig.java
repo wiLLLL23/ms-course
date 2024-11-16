a396373b58b4703fafedc749f567beb4bff645b4
William Barbosa <will.barbosa23@gmail.com>
2024-11-16 01:03:45 -0300

    feat: security configs to config server

--- hr-oauth/src/main/java/com/devwill/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/devwill/hroauth/config/AuthorizationServerConfig.java
@@ -1,5 +1,6 @@
 package com.devwill.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${oauth.client.name}")
+    private String clientName;
+
+    @Value("${oauth.client.secret}")
+    private String clientSecret;
 
     private BCryptPasswordEncoder passwordEncoder;
     private JwtAccessTokenConverter accessTokenConverter;
@@ -40,8 +47,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("myappname123")
-                .secret(passwordEncoder.encode("myappsecret123"))
+                .withClient(clientName)
+                .secret(passwordEncoder.encode(clientSecret))
                 .scopes("read", "write")
                 .authorizedGrantTypes("password")
                 .accessTokenValiditySeconds(86400);
