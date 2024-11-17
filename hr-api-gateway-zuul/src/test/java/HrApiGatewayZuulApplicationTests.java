33b8d460e83cf14e76386006bddcea8278e50b00
William Barbosa <will.barbosa23@gmail.com>
2024-11-17 00:26:21 -0300

    chore: hr-eureka-server container

--- hr-api-gateway-zuul/src/test/java/HrApiGatewayZuulApplicationTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HrApiGatewayZuulApplicationTests {
-
-    @Test
-    void contextLoads() {}
-}
